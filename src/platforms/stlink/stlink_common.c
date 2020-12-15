7e945872a728bf54eb514aa25371654a8340bd2c
Anton Evmenenko <anton.evmenenko.93@gmail.com>
2020-12-15 13:33:06 +0100

    revision hardcoded for bumpy

--- src/platforms/stlink/stlink_common.c
+++ src/platforms/stlink/stlink_common.c
@@ -24,71 +24,72 @@
 /* return 0 for stlink V1, 1 for stlink V2 and 2 for stlink V2.1 */
 uint32_t detect_rev(void)
 {
-	uint32_t rev;
-	int res;
+	return 1;
+	// uint32_t rev;
+	// int res;
 
-	while (RCC_CFGR & 0xf) /* Switch back to HSI. */
-		RCC_CFGR &= ~3;
-	rcc_periph_clock_enable(RCC_GPIOA);
-	rcc_periph_clock_enable(RCC_GPIOB);
-	rcc_periph_clock_enable(RCC_GPIOC);
-	rcc_periph_clock_enable(RCC_USB);
-	rcc_periph_reset_pulse(RST_USB);
-	rcc_periph_clock_enable(RCC_AFIO);
-	rcc_periph_clock_enable(RCC_CRC);
-	/* First, get Board revision by pulling PC13/14 up. Read
-	 *  11 for ST-Link V1, e.g. on VL Discovery, tag as rev 0
-	 *  11 for Baite, PB11 pulled high,          tag as rev 1
-	 *  00 for ST-Link V2, e.g. on F4 Discovery, tag as rev 1
-	 *  01 for ST-Link V2, else,                 tag as rev 1
-	 */
-	gpio_set_mode(GPIOC, GPIO_MODE_INPUT,
-				  GPIO_CNF_INPUT_PULL_UPDOWN, GPIO14 | GPIO13);
-	gpio_set(GPIOC, GPIO14 | GPIO13);
-	gpio_set_mode(GPIOB, GPIO_MODE_INPUT,
-				  GPIO_CNF_INPUT_PULL_UPDOWN, GPIO11);
-	gpio_clear(GPIOB, GPIO11);
-	for (int i = 0; i < 100; i ++)
-		res = gpio_get(GPIOC, GPIO13);
-	if (res)
-		rev = (gpio_get(GPIOB, GPIO11))? 1 : 0;
-	else {
-		/* Check for V2.1 boards.
-		 * PA15/TDI is USE_RENUM, pulled with 10 k to U5V on V2.1,
-		 * Otherwise unconnected. Enable pull low. If still high.
-		 * it is V2.1.*/
-		rcc_periph_clock_enable(RCC_AFIO);
-		AFIO_MAPR |= 0x02000000; /* Release from TDI.*/
-		gpio_set_mode(GPIOA, GPIO_MODE_INPUT,
-                                 GPIO_CNF_INPUT_PULL_UPDOWN, GPIO15);
-		gpio_clear(GPIOA, GPIO15);
-		for (int i = 0; i < 100; i++)
-			res =  gpio_get(GPIOA, GPIO15);
-		if (res) {
-			rev = 2;
-			/* Pull PWR_ENn low.*/
-			gpio_set_mode(GPIOB, GPIO_MODE_OUTPUT_2_MHZ,
-						  GPIO_CNF_OUTPUT_OPENDRAIN, GPIO15);
-			gpio_clear(GPIOB, GPIO15);
-			/* Pull USB_RENUM low!*/
-			gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_2_MHZ,
-						  GPIO_CNF_OUTPUT_OPENDRAIN, GPIO15);
-			gpio_clear(GPIOA, GPIO15);
-		} else
-			/* Catch F4 Disco board with both resistors fitted.*/
-			rev = 1;
-		/* On Rev > 0 unconditionally activate MCO on PORTA8 with HSE! */
-		RCC_CFGR &= ~(0xf << 24);
-		RCC_CFGR |= (RCC_CFGR_MCO_HSE << 24);
-		gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_50_MHZ,
-		GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO8);
-	}
-	if (rev < 2) {
-		gpio_clear(GPIOA, GPIO12);
-		gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_2_MHZ,
-					  GPIO_CNF_OUTPUT_OPENDRAIN, GPIO12);
-	}
-	return rev;
+	// while (RCC_CFGR & 0xf) /* Switch back to HSI. */
+	// 	RCC_CFGR &= ~3;
+	// rcc_periph_clock_enable(RCC_GPIOA);
+	// rcc_periph_clock_enable(RCC_GPIOB);
+	// rcc_periph_clock_enable(RCC_GPIOC);
+	// rcc_periph_clock_enable(RCC_USB);
+	// rcc_periph_reset_pulse(RST_USB);
+	// rcc_periph_clock_enable(RCC_AFIO);
+	// rcc_periph_clock_enable(RCC_CRC);
+	// /* First, get Board revision by pulling PC13/14 up. Read
+	//  *  11 for ST-Link V1, e.g. on VL Discovery, tag as rev 0
+	//  *  11 for Baite, PB11 pulled high,          tag as rev 1
+	//  *  00 for ST-Link V2, e.g. on F4 Discovery, tag as rev 1
+	//  *  01 for ST-Link V2, else,                 tag as rev 1
+	//  */
+	// gpio_set_mode(GPIOC, GPIO_MODE_INPUT,
+	// 			  GPIO_CNF_INPUT_PULL_UPDOWN, GPIO14 | GPIO13);
+	// gpio_set(GPIOC, GPIO14 | GPIO13);
+	// gpio_set_mode(GPIOB, GPIO_MODE_INPUT,
+	// 			  GPIO_CNF_INPUT_PULL_UPDOWN, GPIO11);
+	// gpio_clear(GPIOB, GPIO11);
+	// for (int i = 0; i < 100; i ++)
+	// 	res = gpio_get(GPIOC, GPIO13);
+	// if (res)
+	// 	rev = (gpio_get(GPIOB, GPIO11))? 1 : 0;
+	// else {
+	// 	/* Check for V2.1 boards.
+	// 	 * PA15/TDI is USE_RENUM, pulled with 10 k to U5V on V2.1,
+	// 	 * Otherwise unconnected. Enable pull low. If still high.
+	// 	 * it is V2.1.*/
+	// 	rcc_periph_clock_enable(RCC_AFIO);
+	// 	AFIO_MAPR |= 0x02000000; /* Release from TDI.*/
+	// 	gpio_set_mode(GPIOA, GPIO_MODE_INPUT,
+    //                              GPIO_CNF_INPUT_PULL_UPDOWN, GPIO15);
+	// 	gpio_clear(GPIOA, GPIO15);
+	// 	for (int i = 0; i < 100; i++)
+	// 		res =  gpio_get(GPIOA, GPIO15);
+	// 	if (res) {
+	// 		rev = 2;
+	// 		/* Pull PWR_ENn low.*/
+	// 		gpio_set_mode(GPIOB, GPIO_MODE_OUTPUT_2_MHZ,
+	// 					  GPIO_CNF_OUTPUT_OPENDRAIN, GPIO15);
+	// 		gpio_clear(GPIOB, GPIO15);
+	// 		/* Pull USB_RENUM low!*/
+	// 		gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_2_MHZ,
+	// 					  GPIO_CNF_OUTPUT_OPENDRAIN, GPIO15);
+	// 		gpio_clear(GPIOA, GPIO15);
+	// 	} else
+	// 		/* Catch F4 Disco board with both resistors fitted.*/
+	// 		rev = 1;
+	// 	/* On Rev > 0 unconditionally activate MCO on PORTA8 with HSE! */
+	// 	RCC_CFGR &= ~(0xf << 24);
+	// 	RCC_CFGR |= (RCC_CFGR_MCO_HSE << 24);
+	// 	gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_50_MHZ,
+	// 	GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO8);
+	// }
+	// if (rev < 2) {
+	// 	gpio_clear(GPIOA, GPIO12);
+	// 	gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_2_MHZ,
+	// 				  GPIO_CNF_OUTPUT_OPENDRAIN, GPIO12);
+	// }
+	// return rev;
 }
 
 void platform_request_boot(void)
